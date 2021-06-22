f933777bd5234cef20a231e711236cf27f566509
yugmani <yugmani@yahoo.com>
2021-06-21 21:17:15 -0700

    task 1 and task2: checking for emptiness of an object

---
+++ task.js
@@ -0,0 +1,68 @@
+// *************************
+// TASKS
+// *************************
+
+// https://javascript.info/object#property-names-limitations
+
+// 1. ********** Hello, object *************
+
+// Write the code, one line for each action:
+
+// 1. Create an empty object user.
+
+const user = {};
+// console.log(user); // {}
+// 2. Add the property name with the value John.
+user.name = 'John';
+// console.log(user); // {name: "John"}
+// 3. Add the property surname with the value Smith.
+user.surname = 'Smith';
+// console.log(user); // {name: "John", surname: "Smith"}
+// 4. Change the value of the name to Pete.
+user.name = 'Pete';
+// console.log(user); // {name: "Pete", surname: "Smith"}
+// 5. Remove the property name from the object.
+delete user.name;
+
+// console.log(user); // {surname: 'Smith'}
+
+// ******** 2. Check for emptiness ********
+
+// Write the function isEmpty(obj) which returns true if the object has no properties, false otherwise.
+
+// My solution
+function isObjEmpty(obj) {
+  let prop = Object.keys(obj);
+  // let prop = Object.values(obj); // or
+  // let prop = Object.entries(obj);  // or
+  // let prop = Object.getOwnPropertyNames(obj);  //or
+
+  if (prop.length === 0) {
+    return true;
+  }
+
+  return false;
+}
+
+// Author's solution
+function isEmpty(obj) {
+  for (let key in obj) {
+    // if the loop has started, there is a property
+    return false;
+  }
+
+  return true;
+}
+
+// Should work like that:
+
+let schedule = {};
+
+// alert( isEmpty(schedule) ); // true
+
+schedule['8:30'] = 'get up';
+
+alert(isEmpty(schedule)); // false
+
+// console.log(schedule);
+// console.log(isEmpty(schedule));
